cb44632c5f8ffdca74454f9c81c24bba1c609d9f
ryanemm <59361481+ryanemm@users.noreply.github.com>
2021-08-29 14:55:44 +0200

    schema-studentdetails

---
+++ backend/student_details.sql
@@ -0,0 +1,11 @@
+-- create table for student details
+--
+DROP table IF EXISTS 'student_details';
+CREATE TABLE IF NOT EXISTS 'student_details' (
+	'id' int(11) NOT NULL AUTO_INCREMENT, 
+	'first_name' varchar(20) NOT NULL,
+	'last_name' varchar(20) NOT NULL,
+	'adress' varchar(50),
+	PRIMARY KEY ('id')	
+	);
+
---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Container(
+                child: Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Column(
+            children: [],
+          ),
+        )
+      ],
+    ))));
+  }
+}
